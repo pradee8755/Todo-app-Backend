eb90d295d56f8bb82bb07d42d98e1bfd1aead98a
pradee8755 <pradeep.saddala@gmail.com>
2020-11-08 10:56:37 +0530

    commit

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const env = require('./constants');
 let dbConnection = () => {
     console.log("test", env.userName);
-    mongoose.connect(`mongodb+srv://${env.userName}:${env.pass}@mycluster-al81u.mongodb.net/todo?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }).then(result => {
+    mongoose.connect(``, { useNewUrlParser: true, useUnifiedTopology: true }).then(result => {
         console.log("connection established");
     }).catch(err => {
         console.log(`database error${err}`)
